35c39d7df3ba016f3f36d305443661f535b4213d
Roman Sabechkine <romansabechkine@Atlant.local>
2024-02-27 16:51:30 +0100

    Correct mechanics of tank movement

--- Assets/Scripts/Tank.cs
+++ Assets/Scripts/Tank.cs
@@ -11,9 +11,11 @@ public class Tank : MonoBehaviour
     private Rigidbody rb;
 
     public float speed;
+
     public InputActionReference verticalMovement;
     // Start is called before the first frame update
 
+    public InputActionReference rotationMovement;
     
     void Start()
     {
@@ -24,7 +26,14 @@ public class Tank : MonoBehaviour
     void Update()
     {
         Vector3 movement = this.transform.position;
-        movement += Vector3.forward * speed * verticalMovement.action.ReadValue<float>() * Time.deltaTime;
+        movement += this.transform.forward * speed * verticalMovement.action.ReadValue<float>() * Time.deltaTime;
         rb.MovePosition(movement);
+
+        Vector3 m_EulerAngleVelocity;
+        Quaternion initialRotation = this.transform.rotation;
+        //Set the angular velocity of the Rigidbody (rotating around the Y axis, 100 deg/sec)
+        m_EulerAngleVelocity = new Vector3(0, 2000, 0);
+        Quaternion deltaRotation = Quaternion.Euler(rotationMovement.action.ReadValue<float>() * Time.deltaTime * m_EulerAngleVelocity);
+        rb.MoveRotation( initialRotation * deltaRotation);
     }
 }
