1a8b91dce028dabb4ea48cb08d04e50a9438cbda
Roman Sabechkine <romansabechkine@Atlant.local>
2024-02-27 14:35:58 +0100

    Tank and bullet models and not perfect vertical movement

--- Assets/Scripts/Tank.cs
+++ Assets/Scripts/Tank.cs
@@ -1,18 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.InteropServices.WindowsRuntime;
+using UnityEditor.Callbacks;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Tank : MonoBehaviour
 {
+
+    private Rigidbody rb;
+
+    public float speed;
+    public InputActionReference verticalMovement;
     // Start is called before the first frame update
+
+    
     void Start()
     {
-        
+        rb = this.GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 movement = this.transform.position;
+        movement = Vector3.forward * speed * verticalMovement.action.ReadValue<float>() * Time.deltaTime;
+        rb.MovePosition(movement);
     }
 }
